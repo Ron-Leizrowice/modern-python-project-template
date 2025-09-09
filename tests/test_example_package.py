570624355e617e94b56d458a1d404db38580ef9e
RonLeizrowice-Pelanor <ron@pelanor.io>
2025-09-09 14:29:21 +0100

    Added python CI checks

---
+++ tests/test_example_package.py
@@ -0,0 +1,8 @@
+import importlib.util
+
+import pytest
+
+
+def test_import() -> None:
+    if importlib.util.find_spec("example_package") is None:
+        pytest.fail("Failed to find example_package module")
